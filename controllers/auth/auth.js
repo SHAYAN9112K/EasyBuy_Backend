f5176bfb80a1b5155b73bd83ddd95e7cde015d2b
ehtshamaziz <56515123+ehtshamaziz@users.noreply.github.com>
2023-07-03 21:12:23 +0500

    Added password backend

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -1,16 +1,15 @@
 const userModel = require("../../models/user");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken")
-const JWT_SECRET_KEY = process.env.TOKEN_KEY
+const jwt = require("jsonwebtoken");
+const JWT_SECRET_KEY = process.env.TOKEN_KEY;
 
-function generateAuthToken(data){
-  const token = jwt.sign(data, JWT_SECRET_KEY, { expiresIn: '10h' })
-  return token
+function generateAuthToken(data) {
+  const token = jwt.sign(data, JWT_SECRET_KEY, { expiresIn: "10h" });
+  return token;
 }
 
 module.exports.login = async (req, res) => {
   try {
-
     const { email, password } = req.body;
     let user = await userModel.findOne({ email });
 
@@ -24,10 +23,9 @@ module.exports.login = async (req, res) => {
 
     // bcrypting the password and comparing with the one in db
     if (await bcrypt.compare(password, user.password)) {
-
-      const token = generateAuthToken({_id : user?._id, email : email})
-      user.token = token
-      user.save()
+      const token = generateAuthToken({ _id: user?._id, email: email });
+      user.token = token;
+      user.save();
 
       return res.json({
         success: true,
@@ -37,11 +35,10 @@ module.exports.login = async (req, res) => {
       });
     }
     return res.json({
-        success: false,
-        status: 400,
-        message: "user credentials are not correct",
-    })
-
+      success: false,
+      status: 400,
+      message: "user credentials are not correct",
+    });
   } catch (error) {
     return res.send(error.message);
   }
@@ -69,13 +66,22 @@ module.exports.register = async (req, res) => {
       data: user,
     });
   } catch (error) {
-    return res.send(error.message);
+    if (error.code === 11000) {
+      // Duplicate key error, email already exists
+      return res.json({ message: "Email already exists." });
+      // Handle the error appropriately
+    } else {
+      return res.send(error.message);
+    }
   }
+
+  // catch (error) {
+  //   return res.send(error.message);
+  // }
 };
 
 module.exports.updateUser = async (req, res) => {
   try {
-
     const userDataToBeUpdated = req.body;
     const { id } = req.query;
     const user = await userModel.findOne({ _id: id });
@@ -106,7 +112,7 @@ module.exports.deleteUser = async (req, res) => {
     if (!user) return res.status(200).send("user does not exist");
 
     await userModel.findOneAndDelete({ _id: id });
-    
+
     return res.json({
       success: true,
       message: "user deleted successfully",
@@ -120,50 +126,46 @@ module.exports.userById = async (req, res) => {
   try {
     const { id } = req.query;
 
-    const user = await userModel.findOne({_id : id})
-    if(!user) return res.send("user does not exist")
+    const user = await userModel.findOne({ _id: id });
+    if (!user) return res.send("user does not exist");
 
     return res.json({
-        success : true,
-        message : "user deleted successfully",
-        data : user
-    })
-
-    }catch(error){
-        return res.send("error : ", error.message)
-    }
-}
+      success: true,
+      message: "user deleted successfully",
+      data: user,
+    });
+  } catch (error) {
+    return res.send("error : ", error.message);
+  }
+};
 
 module.exports.resetPassword = async (req, res) => {
+  try {
+    const { password, newPassword } = req.body;
+    const { id } = req.query;
 
-    try{
-        const {password, newPassword} = req.body;
-        const {id} = req.query
-    
-        if(!password || !newPassword || !id) return res.send("Fields are empty")
-    
-        let user = await userModel.findOne({_id : id})
-    
-        if(!user) return res.send("user does not exist")
-    
-        // comparing the password from the password in DB to allow changes
-        if(bcrypt.compare(password, user?.password)){
-            // encrypting new password 
-            user.password = await bcrypt.hash(newPassword,10)
-            user.save()
-            return res.json({
-                success : true,
-                message : "password updated successfully"
-            })
-        }
+    if (!password || !newPassword || !id) return res.send("Fields are empty");
 
-        return res.json({
-            success : false,
-            message : "wrong password"
-        })
+    let user = await userModel.findOne({ _id: id });
 
-    }catch(error){
-        return res.send(error.message)
+    if (!user) return res.send("user does not exist");
+
+    // comparing the password from the password in DB to allow changes
+    if (bcrypt.compare(password, user?.password)) {
+      // encrypting new password
+      user.password = await bcrypt.hash(newPassword, 10);
+      user.save();
+      return res.json({
+        success: true,
+        message: "password updated successfully",
+      });
     }
-    
-}
+
+    return res.json({
+      success: false,
+      message: "wrong password",
+    });
+  } catch (error) {
+    return res.send(error.message);
+  }
+};
